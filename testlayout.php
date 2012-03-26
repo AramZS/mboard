e4e838806ab5ff87e8552fe3761f84791ee03eba
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-03-26 14:12:54 -0400

    testing a layout.

---
+++ testlayout.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+
+<div><!--open container-->
+	<div><!--open inner container-->
+		<div><!--open left tab column-->
+		</div><!--close left tab column-->
+		<div><!--open main content-->
+			<div><!--open main content tabs-->
+			</div><!--close main content tabs-->
+			<div><!--open main content body-->
+				<div><!--open item one-->
+				</div><!--close item one-->
+				<div><!--open item two-->
+				</div><!--close item two-->
+				<div><!--open item three-->
+				</div><!--close item three-->
+				<div><!--open item four-->
+				</div><!--close item four-->
+				<div><!--open item five-->
+				</div><!--close item five-->
+				<div><!--open item six-->
+				</div><!--close item six-->
+			</div><!--close main content body-->
+		</div><!--close main content-->
+	</div><!--close inner container-->
+</div><!--close container-->
+
+</body>
+</html>
